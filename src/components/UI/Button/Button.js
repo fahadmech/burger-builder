5c45de227ce1c58dabcfe39d0ab49f42671a949c
fahadmech <fshaikh349@gmail.com>
2019-07-19 16:19:12 +0530

    completed form validation

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -5,7 +5,7 @@ const button = (props) => (
     <div>
         <button
         className={[classes.Button, classes[props.btnType]].join(" ")}
-        onClick={props.clicked}>
+        onClick={props.clicked} disabled={props.disabled}>
             {props.children}
         </button>
     </div>
